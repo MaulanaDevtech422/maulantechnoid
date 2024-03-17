9967d8e0bc3206b40a4ea37b1c8860bcfda05fdb
MaulanaDevtech422 <163699226+MaulanaDevtech422@users.noreply.github.com>
2024-03-17 08:54:16 +0700

    Create Main.sh
    Ini adalah script prompt bash termux

---
+++ Main.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Clear the terminal screen
+clear
+red='\033[0;35m'
+green='\033[0;32m'
+r="\033[0;31m"
+y="\033[0;36m"
+t="\033[0;33m"
+# Print the title using figlet
+
+figlet -f slant "MaulanaDevtech" | lolcat
+./b.sh | lolcat
+# Print the bio description
+echo -e "${y}++++++++++++++++++++++++++++++++"
+echo -e "${y}+         ${t} contac us         ${y}  +"
+echo -e "${y}++++++++++++++++++++++++++++++++"
+echo -e "${y}+ ${t}Gmail  :technocode@gmail.com ${y}+"
+echo -e "+ ${t}No Tel :+6285712816240      ${y} +"
+echo -e "${y}++++++++++++++++++++++++++++++++"
+echo ""
+echo -e "${green}----------------------------------------------------------------------------------->
+echo -e "-                                ${green}~ABOUT MAULANADEVTECH~                            >
+echo "----------------------------------------------------------------------------------------"
+echo -e "${red}- [÷]Web Developer and Back End Developer specializing in Python                     >
+echo "- with a passion for creating robust and scalable web applications.                    -"
+echo "- [÷]Experienced in Django, FastAPI, Flask, HTML, CSS, and JavaScript                  -"
+echo "- [÷]React, Node.js, and Express.js. I am also familiar with                           -"
+echo "- database management systems like MongoDB and PostgreSQL.                             -"
+echo "- [÷]Always eager to learn and adapt to new technologies and trends in web development.-"
+echo "- [÷]saya memiliki pengetahuan yang baik tentang konsep-konsep dasar Python            -"
+echo "- seperti OOP, listcomprehension, dan generator.                                       -"
+echo "- [÷]I am also familiar with several tools and technologies commonly used in the       -"
+echo "- web development world, such as HTML, CSS, JavaScript, Git, and Docker.               -"
+echo "----------------------------------------------------------------------------------------"
